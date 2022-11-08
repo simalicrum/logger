e9561dbc82fd003fbcabf33930f4a951a358aa71
Robert Reinert <robert.reinert@gmail.com>
2022-11-08 11:29:29 -0800

    Export default config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import winston from 'winston';
-import { logConfiguration } from './config/winston.js';
+
+export { logConfiguration } from './config/winston.js';
 
 export const createLogger = (filename) => {
   const logger = winston.createLogger(logConfiguration(filename));
