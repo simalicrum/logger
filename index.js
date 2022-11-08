53b0d09ff23a2bb6647394de08915326e93f2abb
Robert Reinert <robert.reinert@gmail.com>
2022-11-08 11:35:43 -0800

    regress logConfiguration error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import winston from 'winston';
 
-export { logConfiguration } from './config/winston.js';
+import { logConfiguration } from './config/winston.js';
 
 export const createLogger = (filename) => {
   const logger = winston.createLogger(logConfiguration(filename));
